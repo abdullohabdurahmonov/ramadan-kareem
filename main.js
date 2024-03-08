74f01d0b04a1ca51ef3e7a38a4465b4edceaac75
Tony Stark (lux copy) <158560749+abdullohabdurahmonov@users.noreply.github.com>
2024-03-08 09:52:41 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+const date = document.querySelector('#date')
+const date2 = document.querySelector('#date2')
+const month1 = document.querySelector('month1')
+const year1 = document.querySelector('year1')
+
+const now = new Date()
+
+const today = now.getDate() < 10 ? '0' + now.getDate() : now.getDate()
+const month = now.getMonth()
+const year = now.getFullYear()
+
+const months = [
+    'Yanvar',
+    'Fevral',
+    'Mart',
+    'Aprel',
+    'May',
+    'Iyun',
+    'Iyul',
+    'Avgust',
+    'Sentabr',
+    'Oktabr',
+    'Noyabr',
+    'Dekabr',
+] 
+
+
+
+date.innerHTML = `${today}${months[month]}.${year}`
+date2.innerHTML = `${today}${months[month]}.${year}`
+
+
+
+
+
+
